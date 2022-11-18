aa1f07306906340d7044efe25e44f3ae04584ab1
jinwenjie123 <jinwenjie00@gmail.com>
2022-11-17 16:22:36 -0800

    get selected text and send to background.js

--- frontend/extension/background.js
+++ frontend/extension/background.js
@@ -4,3 +4,9 @@ chrome.action.onClicked.addListener((tab) => {
         files: ["content.js"],
     });
 });
+
+chrome.runtime.onMessage.addListener(receiver);
+
+function receiver(request, sender, sendResponse){
+    console.log(request); 
+}
