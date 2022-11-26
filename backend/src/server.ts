a61f070b7099171cadedd3cc3854acafe8bfdac1
sauravbanna <sauravbanna@gmail.com>
2022-11-26 00:33:52 -0800

    -

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,33 +1,12 @@
 import express from "express";
 import cors from "cors";
+import textExtractor from "./textExtractor";
 const app = express();
 
 app.use(express.json());
 
 app.use(cors());
 
-app.post("/summary", (req: express.Request, res: express.Response) => {
-    if (!req.body || !req.body.content || !req.body.length) {
-        res.status(400).send("Invalid Request - Please supply some text and length to summarize.");
-    } else {
-        res.setTimeout(60000, () => {
-            res.status(504).send("Server Timed Out.");
-        });
-
-        try {
-            // TODO Call Python API
-        } catch (e: any) {}
-
-        res.status(200).send({
-            summarized_text: "Placeholder",
-            meta: {
-                length: 11,
-            },
-        });
-    }
-
-
-});
-
+app.use("/", textExtractor);
 
 export default app;
