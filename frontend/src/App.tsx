ad6bc2d45883eae850ff0aa0357111c24370d92f
ryanmckim <mck649@gmail.com>
2022-11-27 16:51:11 -0800

    Configured seperate SCSS files for organization.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import { useState } from 'react';
 import ArticleSection from './components/ArticleSection/Summary';
 import Article from './components/ArticleSection/index';
--- frontend/src/Article.tsx
+++ frontend/src/Article.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import './App.css';
+import './App.scss';
 
 interface IArticleData {
     title: string,
