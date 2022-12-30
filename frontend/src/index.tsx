8163c6406c2bbbe18f7b9dc88000bf099c61516a
sauravbanna <sauravbanna@gmail.com>
2022-12-29 23:26:39 -0800

    implemented button to change extension theme state, and implemented scss to reflect this change according to Figma design

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,8 @@
 import App from './App';
+import ThemeProvider from './context/ThemeContent'
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container!);
-root.render(<App />);
+root.render(<ThemeProvider>
+    <App />
+</ThemeProvider>);
