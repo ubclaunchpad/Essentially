ea0055fea1d08dd12051b8824fd68da6daebf741
Gordon Cheung <gordonhwc1@gmail.com>
2023-02-22 15:18:46 -0800

    initial Python code refactor

---
+++ summary_be/lambda_handlers/summary.py
@@ -0,0 +1,32 @@
+from utils.summarization import summarize
+
+def get_summary(event, context):
+    text, num_sentences = event["content"]["text"], event["length"] or 3
+
+    if not text:
+        return {
+            "statusCode": 400,
+            "message": "Empty Text!"
+        }
+
+    if type(text) is not str or type(num_sentences) is not int:
+        return {
+            "statusCode": 400,
+            "message": "Bad Input Type!"
+        }
+
+    try:
+        result = summarize(text, num_sentences)
+        num_words = len(result.split(" "))
+        return {
+            "statusCode": 200,
+            "summarized_text": result, 
+            "Meta": {
+                "length": num_words
+            }
+        }
+    except:
+        return {
+            "statusCode": 500,
+            "message": "Unknown Error!"
+        }
