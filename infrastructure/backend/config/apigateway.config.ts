6a1a7f45a2f416efe77e1a606872c1bedab0ffc3
michaelma <taomaqueens@gmail.com>
2023-02-21 21:02:37 -0800

    CDK implementation for API gateway and lambda

---
+++ infrastructure/backend/config/apigateway.config.ts
@@ -0,0 +1,29 @@
+import { API_GATEWAY_METHOD, COMPONENTS } from "./constants";
+
+export interface ApiGatewayMethodConfig {
+  componentName: string,
+  methodType: string,
+  requestTemplates: {[contentType: string]: string},
+}
+
+export const REQUEST_TEMPLATES = { "application/json": '{ "statusCode": "200" }' };
+
+const BackendStatusAPI: ApiGatewayMethodConfig = {
+  componentName: COMPONENTS.status,
+  methodType: API_GATEWAY_METHOD.GET,
+  requestTemplates: REQUEST_TEMPLATES,
+}
+
+const SummaryAPI: ApiGatewayMethodConfig = {
+  componentName: COMPONENTS.summary,
+  methodType: API_GATEWAY_METHOD.POST,
+  requestTemplates: REQUEST_TEMPLATES,
+}
+
+const KeywordAPI: ApiGatewayMethodConfig = {
+  componentName: COMPONENTS.keyword,
+  methodType: API_GATEWAY_METHOD.POST,
+  requestTemplates: REQUEST_TEMPLATES,
+}
+
+export const TO_DEPLOY_APIS: ApiGatewayMethodConfig[] = [BackendStatusAPI, SummaryAPI, KeywordAPI];
--- infrastructure/backend/config/constants.ts
+++ infrastructure/backend/config/constants.ts
@@ -1,3 +1,20 @@
 export const APP_NAME = {
   PREFIX: "Essentially-Backend-"
 }
+
+export const COMPONENTS = {
+  status: "status",
+  summary: "summary",
+  keyword: "keyword",
+}
+
+export const RESOURCE_ID = "ID";
+
+export const LAMBDA_NAMES = {
+  function: "function"
+}
+
+export const API_GATEWAY_METHOD = {
+  GET: "GET",
+  POST: "POST",
+}
