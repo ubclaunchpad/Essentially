035e3db7c0c83a3286ee5d338297f8807bbaedfb
Gordon Cheung <gordonhwc1@gmail.com>
2023-02-22 15:52:06 -0800

    set function name and path

--- infrastructure/summary_be/config/constants.ts
+++ infrastructure/summary_be/config/constants.ts
@@ -17,3 +17,10 @@ export const API_GATEWAY_METHOD = {
   GET: "GET",
   POST: "POST",
 };
+
+export const SUMMARY_BE_LAMBDA_HANDLERS = {
+  summary: "get_summary",
+  status: "get_status",
+};
+
+export const SUMMARY_BE_BASE_DIR = "/../../../summary_be/lambda_handlers";
--- infrastructure/summary_be/config/lambda.config.ts
+++ infrastructure/summary_be/config/lambda.config.ts
@@ -1,6 +1,11 @@
 import { Runtime } from "aws-cdk-lib/aws-lambda";
-import { COMPONENTS, LAMBDA_NAMES } from "./constants";
-
+import {
+  COMPONENTS,
+  LAMBDA_NAMES,
+  SUMMARY_BE_BASE_DIR,
+  SUMMARY_BE_LAMBDA_HANDLERS,
+} from "./constants";
+import * as path from "path";
 export interface LambdaConfig {
   componentName: string;
   functionName: string;
@@ -9,23 +14,20 @@ export interface LambdaConfig {
   runtime: Runtime;
 }
 
-export const BACKEND_SAMPLE_LAMBDA_ASSET_PATH = "resources";
-export const BACKEND_SAMPLE_LAMBDA_HANDLER = "sample_lambda.main";
-
 const BackendStatus: LambdaConfig = {
   componentName: COMPONENTS.status,
   functionName: COMPONENTS.status + LAMBDA_NAMES.function,
-  asset: BACKEND_SAMPLE_LAMBDA_ASSET_PATH,
-  handler: BACKEND_SAMPLE_LAMBDA_HANDLER,
-  runtime: Runtime.NODEJS_14_X,
+  asset: path.join(__dirname, SUMMARY_BE_BASE_DIR),
+  handler: COMPONENTS.status + "." + SUMMARY_BE_LAMBDA_HANDLERS.status,
+  runtime: Runtime.PYTHON_3_9,
 };
 
 const Summary: LambdaConfig = {
   componentName: COMPONENTS.summary,
   functionName: COMPONENTS.summary + LAMBDA_NAMES.function,
-  asset: BACKEND_SAMPLE_LAMBDA_ASSET_PATH,
-  handler: BACKEND_SAMPLE_LAMBDA_HANDLER,
-  runtime: Runtime.NODEJS_14_X,
+  asset: path.join(__dirname, SUMMARY_BE_BASE_DIR),
+  handler: COMPONENTS.summary + "." + SUMMARY_BE_LAMBDA_HANDLERS.summary,
+  runtime: Runtime.PYTHON_3_9,
 };
 
 export const BACKEND_TO_DEPLOY_LAMBDAS: LambdaConfig[] = [
